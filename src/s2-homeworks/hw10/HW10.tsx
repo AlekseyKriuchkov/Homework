ebafdd45c4d11a3da95dcbdef08651db079eefb9
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2023-02-19 15:45:11 +0300

    HomeWork 11 finished

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -19,7 +19,7 @@ const HW10 = () => {
 
     const dispatch = useDispatch()
     const isLoading = useSelector<AppStoreType, boolean >(state => state.loading.isLoading)
-    console.log(isLoading)
+
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
